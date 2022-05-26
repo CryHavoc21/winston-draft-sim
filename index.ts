baad96c84164b7b9c0b03dd8821655fcef0578ec
CryHavoc21 <cryhavoc24@gmail.com>
2022-05-25 21:48:02 -0500

    added the views and shit

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
 // hello world
 
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
-const express = require ('express');
-const app = express()
-const port = 3000
+const index_path:string = "/home/cryhavoc21/winston_draft/views/index.hmtl";
 
+
+
+app.engine('.html', require('ejs').__express);
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.render('index');
+  //res.send('Hello world!');
 });
-
 
 app.listen(port, () => {
   console.log(`Example application listening on port ${port}`);
